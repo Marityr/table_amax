59cd96c82201167de060c043183cac2b130d19ad
Marityr <dmitriy.woronow@gmail.com>
2022-01-12 11:02:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,31 +24,40 @@ class Exelfile:
 
         return brand_list
 
-    def read_file_sheet(rowit):
+    def read_file_sheet():
         sheet_list = load_workbook('export.xlsx')
         sheet = sheet_list["sheet"]
 
-        tmp = sheet.cell(row=rowit, column=3).value
-        
-        return tmp
+        #tmp = sheet.cell(row=rowit, column=3).value
+        number_list = list()
+        item = 0
+        for cell in sheet['E']:
+            number_list.append(cell.value)
+            item += 1
+            if item >= 1000:
+                break
+        return number_list
 
 class API_set:
     """Взаимодействие с API сервиса"""
 
-    def connect_api(rowit):
+    def connect_api(number_list):
         """Подключение по API и получение json"""
 
         HOST_API = os.getenv('HOST_API')
         USER_API = os.getenv('USER_API')
         PASSWORD_API = os.getenv('PASSWORD_API')
 
-        number = Exelfile.read_file_sheet(rowit)
-
-        conect_url = f"https://{HOST_API}/search/brands/?userlogin={USER_API}&userpsw={PASSWORD_API}&number={number}"
-
-        response = requests.get(conect_url)
-        datajson = response.json()
-        return datajson
+        listen = list()
+        items = 0
+        for i in number_list:
+            conect_url = f"https://{HOST_API}/search/brands/?userlogin={USER_API}&userpsw={PASSWORD_API}&number={i}"
+            response = requests.get(conect_url)
+            datajson = response.json()
+            listen.append(datajson)
+            items += 1
+            print(items)
+        return listen
 
 
 
@@ -60,8 +69,12 @@ def max_row() -> int:
     nb_row = sheet.max_row
     return int(nb_row)
 
-# print(Exelfile.read_file_sheet())
+listen = Exelfile.read_file_sheet()
+listen.pop(0)
+#print(listen)
+tempel = API_set.connect_api(listen)
+print(tempel)
 # print(len(API_set.connect_api()))
 
-for i in range(2, 10):
-    print(len(API_set.connect_api(i)))
+# for i in range(2, 10):
+#     print(len(API_set.connect_api(i)))
